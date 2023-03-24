5f3763a180909d4c2f7873d6d3e0496af89411ab
Fadi Hamad <fadihamad1994@gmail.com>
2023-03-24 17:52:37 -0400

    First attempt to support running multiple threads at the same time where each thread will be executing the benchamrk for a batch of the CUTest problems.

--- scripts/solve_cutest_in_parallel.jl
+++ scripts/solve_cutest_in_parallel.jl
@@ -98,51 +98,31 @@ function restoreParsed_argsAsString(parsed_args::Dict{String, Any})
   end
   return parsed_args_str
 end
-function main()
-  # parsed_args = parse_command_line()
-  # parsed_args_str = strip(restoreParsed_argsAsString(parsed_args))
-  # train_batch_count = Threads.nthreads()
-  # run(Cmd(`julia ../CAt-Journal/scripts/solve_cutest.jl $parsed_args_str`));
-  # Threads.@threads for train_batch_index in 1:1
-  #   #run(Cmd(`julia ./src/test_julia_script.jl $parsed_args_str --train_batch_index $train_batch_index --train_batch_count $train_batch_count`));
-  #   @show "$parsed_args_str --train_batch_index $train_batch_index --train_batch_count $train_batch_count"
-  #   run(Cmd(`julia ../CAt-Journal/scripts/solve_cutest.jl $parsed_args_str`));
-  #   # run(Cmd(`julia ../CAt-Journal/scripts/solve_cutest.jl --ω 8.0 --output_dir test --δ 0.0 --max_it 10000 --min_nvar 1 --solver CAT --tol_opt 1.0e-5 --max_time 1800.0 --default_problems true --r_1 1.0 --γ_2 0.8 --θ 0.1 --β 0.1 --max_nvar 500`))
-  # end
 
-  # folder_name = parsed_args["output_dir"]
-  # if_mkpath("$folder_name")
-  default_problems = parsed_args["default_problems"]
-  min_nvar = 0
-  max_nvar = 0
-  if !default_problems
-    min_nvar = parsed_args["min_nvar"]
-    max_nvar = parsed_args["max_nvar"]
-  end
-  max_it = parsed_args["max_it"]
-  max_time = parsed_args["max_time"]
-  tol_opt = parsed_args["tol_opt"]
-  r_1 = parsed_args["r_1"]
-
-  Threads.@threads for train_batch_index in 1:train_batch_count
-    folder_name = str(parsed_args["output_dir"], "_$train_batch_index")
-    if_mkpath("$folder_name")
-    if parsed_args["solver"] == "CAT" || parsed_args["solver"] == "CAT_GALAHAD_FACTORIZATION" || parsed_args["solver"] == "CAT_GALAHAD_ITERATIVE"
-      θ = parsed_args["θ"]
-      β = parsed_args["β"]
-      ω = parsed_args["ω"]
-      γ_2 = parsed_args["γ_2"]
-      δ = parsed_args["δ"]
-      run_cutest_with_CAT(folder_name, default_problems, max_it, max_time, tol_opt, θ, β, ω, γ_2, r_1, δ, min_nvar, max_nvar, train_batch_count, train_batch_index, parsed_args["solver"])
-    elseif parsed_args["solver"] == "NewtonTrustRegion"
-      run_cutest_with_newton_trust_region(folder_name, default_problems, max_it, max_time, tol_opt, r_1, min_nvar, max_nvar)
-    elseif parsed_args["solver"] == "ARC"
-      run_cutest_with_arc(folder_name, default_problems, max_it, max_time, tol_opt, r_1, min_nvar, max_nvar, train_batch_count, train_batch_index)
-    elseif parsed_args["solver"] == "TRU_GALAHAD_FACTORIZATION" || parsed_args["solver"] == "TRU_GALAHAD_ITERATIVE"
-      run_cutest_with_tru(folder_name, default_problems, max_it, max_time, tol_opt, r_1, min_nvar, max_nvar, train_batch_count, train_batch_index, parsed_args["solver"])
+function getParsedArgsAsArray(parsed_args::Dict{String, Any}, train_batch_count::Int64, train_batch_index::Int64)
+  parsed_args_str_vector = Vector{String}()
+  for (key, value) in parsed_args
+    push!(parsed_args_str_vector, "--$key")
+    if key == "output_dir"
+      push!(parsed_args_str_vector, "$(value)_$train_batch_index")
     else
-      error("`solver` arg must be `CAT`, `CAT_GALAHAD_FACTORIZATION`, `CAT_GALAHAD_ITERATIVE`, `NewtonTrustRegion`, `ARC`, `TRU_GALAHAD_FACTORIZATION`, or `TRU_GALAHAD_ITERATIVE`.")
+      push!(parsed_args_str_vector, "$value")
     end
+  end
+  push!(parsed_args_str_vector, "--train_batch_count")
+  push!(parsed_args_str_vector, "$train_batch_count")
+  push!(parsed_args_str_vector, "--train_batch_index")
+  push!(parsed_args_str_vector, "$train_batch_index")
+  return parsed_args_str_vector
+end
+
+function main()
+  parsed_args = parse_command_line()
+  # parsed_args_str = strip(restoreParsed_argsAsString(parsed_args))
+  train_batch_count = Threads.nthreads()
+  Threads.@threads for train_batch_index in 1:train_batch_count
+    parsedArgsAsStringArray = getParsedArgsAsArray(parsed_args, train_batch_count, train_batch_index)
+    run(Cmd(`julia ../CAt-Journal/scripts/solve_cutest.jl $parsedArgsAsStringArray`));
   end
 end
 
